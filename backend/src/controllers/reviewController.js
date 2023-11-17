c1958d0d10b50f434ee36ef329e79752f7969abd
simon-bertram <simonbertram@hotmail.com>
2023-11-17 17:09:51 +0000

    Complete review controller functionality

--- backend/src/controllers/reviewController.js
+++ backend/src/controllers/reviewController.js
@@ -6,21 +6,71 @@ import User from '../models/userModel.js';
 // route   GET /api/reviews/:name
 // access  Public
 const getReview = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "Get review" });
+  const { name } = req.params;
+  const review = await Review.findOne({ name });
+
+  if (review) {
+    res.status(200).json(review);
+  } else {
+    res.status(404);
+    throw new Error("Review not found");
+  }
 });
 
 // @desc   Create a review
 // route   POST /api/reviews/:name
 // access  Private
 const createReview = asyncHandler(async (req, res) => {
-  res.status(201).json({ message: "Create review" });
+  const { name, title, rating, tags, content } = req.body;
+  const urlName = title.split(" ").join("-").toLowerCase().trim();
+  const review = await Review.create({
+    name: urlName,
+    title,
+    rating,
+    tags,
+    content,
+  });
+  if (review) {
+    res.status(201).json(review);
+  } else {
+    res.status(400).send("Invalid review data");
+  }
 });
 
 // @desc   Upvote a review
 // route   PUT /api/reviews/:name/upvote
 // access  Private
 const upvoteReview = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "Upvote review" });
+  try {
+    const { name } = req.params;
+    const review = await Review.findOne({ name });
+
+    if (!review) {
+      return res.status(404).json({ message: "Review not found" });
+    }
+
+    // Get the user's ID from the request
+    const userId = req.user._id;
+    if (!userId) {
+      return res.status(401).json({ message: "You must be logged in to upvote a review" });
+    }
+
+    // Check if the user has already upvoted this review
+    if (review.upvotes.includes(userId)) {
+      return res.status(400).json({ message: "You've already upvoted this review" });
+    }
+
+    // Add the user's ID to the upvotes array
+    review.upvotes.push(userId);
+
+    // Save the updated review
+    await review.save();
+
+    res.status(200).json({ message: `The ${name} review now has ${review.upvotes.length} upvotes!` });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error" });
+  }
 });
 
 export {
--- backend/src/server.js
+++ backend/src/server.js
@@ -4,6 +4,7 @@ import express, { urlencoded } from "express";
 import cors from "cors";
 import connectDB from "./config/db.js";
 import userRoutes from "./routes/userRoutes.js";
+import reviewRoutes from "./routes/reviewRoutes.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import { cookie } from "express-validator";
 import cookieParser from "cookie-parser";
@@ -24,6 +25,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use('/api/users', userRoutes);
+app.use('/api/reviews', reviewRoutes);
 app.use(notFound);
 app.use(errorHandler);
 
