5761ca3aec0919d5fb34c1c3b01e5e5b3b81b3af
simon-bertram <simonbertram@hotmail.com>
2023-11-24 18:19:21 +0000

    Style upvote button

--- frontend/src/pages/ReviewPage.jsx
+++ frontend/src/pages/ReviewPage.jsx
@@ -14,7 +14,6 @@ const ReviewPage = () => {
         const response = await axios.get(`http://localhost:8000/api/reviews/${reviewId}`);
         const newReviewInfo = response.data;
         setReviewInfo(newReviewInfo);
-        console.log(newReviewInfo);
       } catch (error) {
         console.log("Error fetching review", error);
       }
@@ -29,7 +28,14 @@ const ReviewPage = () => {
     <article className="container mx-auto -mt-20">
       <h1 className="text-5xl text-center font-bold mb-7">{reviewInfo.title}</h1>
       <div className="max-w-prose mx-auto">
-        <p className="mb-5">This review has {reviewInfo.upvotes.length} upvotes.</p>
+        <p className="mb-5 flex items-center">
+          {reviewInfo.upvotes.length} upvote
+          <button className="rounded-full p-1 ml-4 bg-teal hover:bg-gray-400">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M12 19.5v-15m0 0l-6.75 6.75M12 4.5l6.75 6.75" />
+          </svg>
+          </button>
+        </p>
         {reviewInfo.content.map((paragraph, index) => (
           <p key={index} className="mb-4">{paragraph}</p>
         ))}
